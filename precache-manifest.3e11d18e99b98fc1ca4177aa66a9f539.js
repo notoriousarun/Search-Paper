7aaa460141538c82e96313a0d54b07e199cb3640
notoriousarun <notoriousarun@protonmail.com>
2020-09-18 14:04:14 +0530

    Updates

---
+++ precache-manifest.3e11d18e99b98fc1ca4177aa66a9f539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1a220dc6b08278c3e69524c466e267",
+    "url": "/search-papers/index.html"
+  },
+  {
+    "revision": "dd3ba57161cc7b4969c3",
+    "url": "/search-papers/static/css/main.ff909590.chunk.css"
+  },
+  {
+    "revision": "e415d57b6327de1b16c8",
+    "url": "/search-papers/static/js/2.68298d9f.chunk.js"
+  },
+  {
+    "revision": "dd3ba57161cc7b4969c3",
+    "url": "/search-papers/static/js/main.10f58b1e.chunk.js"
+  },
+  {
+    "revision": "638b1538d5817d978a65",
+    "url": "/search-papers/static/js/runtime~main.7dfb4e62.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SEARCH-PAPER/precache-manifest.997644fddc6cdb0864f11e66af1cd241.js"
+  "/search-papers/precache-manifest.3e11d18e99b98fc1ca4177aa66a9f539.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SEARCH-PAPER/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/search-papers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
